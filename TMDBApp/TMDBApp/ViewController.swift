767c62c87d56de4d6375c0c37304f0eb1526675a
Tan Tran <tantt.dev@gmail.com>
2022-04-07 20:44:10 -0400

    Init project

---
+++ TMDBApp/TMDBApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBApp
+//
+//  Created by Tan Tan on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
