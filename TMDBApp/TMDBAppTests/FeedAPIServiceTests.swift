bb738a6ee3fc6d7112c215f214a6ce8993e2ddaf
Tan Tran <tantt.dev@gmail.com>
2022-04-11 11:46:57 -0400

    Test `fetchPopularMovies` success response JSON with list movie

--- TMDBApp/TMDBAppTests/FeedAPIServiceTests.swift
+++ TMDBApp/TMDBAppTests/FeedAPIServiceTests.swift
@@ -25,8 +25,38 @@ class FeedAPIServiceTests: XCTestCase {
             exp.fulfill()
         }
         
-        let emptyPage = makeMovie(items: [])
+        let emptyPage = makeListMovie(items: [])
         let json = makeItemsJSONData(for: emptyPage.json)
+        client.completeWith(data: json)
+        wait(for: [exp], timeout: 2.0)
+    }
+    
+    func test_fetchPopularMovies_responseJSONMovieList() {
+        let client = HTTPClientSpy()
+        let sut = FeedAPIService(httpClient: client)
+                
+        let expectedItems = (0..<5).map { index in makeMovie(id: index, title: "title", posterPath: "poster")}
+
+        let feed = makeListMovie(items: expectedItems)
+        let json = makeItemsJSONData(for: feed.json)
+        
+        let exp = expectation(description: "Wait for loading")
+        sut.fetchPopularMovies(page: 1) { response in
+            switch response {
+            case let .success(items):
+                
+                for (index, movie) in items.enumerated() {
+                    XCTAssertEqual(movie.id, expectedItems[index].model.id)
+                    XCTAssertEqual(movie.title, expectedItems[index].model.title)
+                    XCTAssertEqual(movie.overview, expectedItems[index].model.overview)
+
+                }
+            case .failure:
+                XCTFail("Failure")
+            }
+            exp.fulfill()
+        }
+        
         client.completeWith(data: json)
         wait(for: [exp], timeout: 2.0)
     }
@@ -43,7 +73,23 @@ class FeedAPIServiceTests: XCTestCase {
         }
     }
     
-    func makeMovie(items: [(model: Movie, json: [String : Any])] = [], page: Int = 1) -> (model: Movie, json: [String: Any]) {
+    func makeMovie(id: Int, title: String? = nil, posterPath: String? = nil ) -> (model: Movie, json: [String: Any]) {
+        let model = Movie(id: id,
+                          title: "a title",
+                          overview: "over view",
+                          poster_path: "path", backdrop_path: nil, release_date: nil, revenue: nil)
+
+      let json: [String: Any] = [
+        "id": model.id,
+        "title": model.title,
+        "overview": model.overview,
+        "poster_path": model.poster_path
+      ]
+
+      return (model, json)
+    }
+    
+    func makeListMovie(items: [(model: Movie, json: [String : Any])] = [], page: Int = 1) -> (model: Movie, json: [String: Any]) {
 
       let model = Movie(id: 0,
                         title: "a title",
